f0b8764cdccddb1f9f03a5f626570e49321d5bbf
RamonJales <ramonjales123@gmail.com>
2022-07-31 14:42:20 -0300

    First commit

---
+++ src/main/java/com/br/webservicespringjpaproject/WebserviceSpringJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.br.webservicespringjpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceSpringJpaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceSpringJpaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/webservicespringjpaproject/WebserviceSpringJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.webservicespringjpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceSpringJpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
